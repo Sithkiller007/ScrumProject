c2206c2654150bfd04f2662cc6211da4846e0a61
Daniel Deuringer <s1dadeur@uni-bayreuth.de>
2017-11-22 13:51:47 +0100

    Visualisierung der GegnerRaycasts implementiert

---
+++ 2D_3D_Test/Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (EnemyFieldofView))]
+public class FieldOfViewEditor : Editor {
+
+	void OnSceneGUI()
+    {
+        EnemyFieldofView fow = (EnemyFieldofView)target;
+        Handles.color = Color.white;
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, viewAngleA, fow.viewAngle, fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach(Transform visibleTarget in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+        }
+    }
+}
