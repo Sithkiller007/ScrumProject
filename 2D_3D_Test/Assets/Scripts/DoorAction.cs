74ebcca77f5bd5fe1d062aa895c3a089eb23a27d
Daniel Deuringer <s1dadeur@uni-bayreuth.de>
2017-11-30 14:59:29 +0100

    Türfunktion implementiert

---
+++ 2D_3D_Test/Assets/Scripts/DoorAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAction : MonoBehaviour {
+
+    public bool isOpen;
+    /*private Collider mycol;
+    private Renderer myren;*/
+    //public string Key;
+	public float force;
+	private Rigidbody rigidbod;
+    private void Start()
+    {
+		rigidbod = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            this.isOpen = true;
+        }
+    }
+
+    void Update () {
+		if(this.isOpen)
+        {
+			rigidbod.AddForce(-transform.forward * force, ForceMode.Acceleration);
+			rigidbod.useGravity = true;
+        }
+		if (transform.rotation.y <= -0.9)
+		{
+			rigidbod.useGravity = false;
+			rigidbod.isKinematic = true;
+		}
+	}
+}
