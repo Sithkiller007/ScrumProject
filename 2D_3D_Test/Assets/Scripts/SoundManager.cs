376398be07d2bfed54c974de5913f8b918bf58cb
Sven Berzellis <Sven.Berzellis@t-online.de>
2018-02-04 22:23:39 +0100

    Sound-Effekte eingefügt - Teil 1
    Da es gewisse Probleme beim erstellen eines "SoundManagers" gibt, lade ich zur Sicherheit die bisherigen Ergebnisse hoch.

---
+++ 2D_3D_Test/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    
+    private CharMove moveScript;
+    public AudioClip walljumpSoundClip;
+    public AudioClip jumpSoundClip;
+    public AudioClip musicClip;
+
+    public float Volume;
+    AudioSource audio;
+    public AudioSource MusicSource;
+    public AudioSource walljumpSource;
+    public AudioSource jumpSource;
+
+
+    void Awake()
+    {
+        moveScript = GetComponent<CharMove>();  
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+
+       audio = GetComponent<AudioSource>();
+        MusicSource.clip = musicClip;
+        walljumpSource.clip = walljumpSoundClip;
+        jumpSource.clip = jumpSoundClip;
+
+        MusicSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /* if (moveScript.wallJump =! false)
+         {
+             //audio.PlayOneShot(walljumpSoundClip, Volume);
+             walljumpSource.Play();
+         }
+         if (moveScript.isGrounded == true)
+         {
+             //audio.PlayOneShot(jumpSoundClip, Volume);
+             jumpSource.Play();
+         }
+         // audio.PlayOneShot(SoundEffect, Volume); */
+    }
+}
