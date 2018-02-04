8957337488ef345cd09b9043ee8c69d1135cc25d
Sven Berzellis <bt702653@uni-bayreuth.de>
2018-02-04 12:50:26 +0100

    Letztes Level - Fertig

---
+++ 2D_3D_Test/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour {
+
+    public AudioClip SoundEffect;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audio = GetComponent<AudioSource>();
+	}
+
+    void OnTriggerEnter()
+    {
+       if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundEffect, Volume);
+           // alreadyPlayed = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
