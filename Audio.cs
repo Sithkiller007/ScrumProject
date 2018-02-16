2120f191cff01b48bf93c8c4aad4a47533549d32
AitchEmX <33596694+AitchEmX@users.noreply.github.com>
2018-02-16 15:34:50 +0100

    Audio Skript separat

---
+++ Audio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+
+    public AudioSource playAudio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            AudioSource[] sources = player.GetComponentsInChildren<AudioSource>();
+            foreach(AudioSource source in sources)
+            {
+                if (source.isPlaying)
+                    source.Stop();
+            }
+
+            playAudio.Play();
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+}
