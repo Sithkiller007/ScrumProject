a7366ae4e1e07cf33f3d1572074ad594aaab2045
Daniel <s1dadeur@uni-bayreuth.de>
2018-02-23 15:14:36 +0100

    Level 2.2 kamera fixiert

--- 2D_3D_Test/Assets/Scripts/SideCameraFollow.cs
+++ 2D_3D_Test/Assets/Scripts/SideCameraFollow.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SideCameraFollow : MonoBehaviour {
 
     public Transform player;
+    bool normal;
+    float nullpos;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        nullpos = player.position.y + 3.5f;
+        Scene actScene = SceneManager.GetActiveScene();
+        //statt 16 bitte den BuildIndex von Level 2_2 einfügen
+        if (actScene.buildIndex == 16)
+        {
+            normal = false;
+        }
+        else
+        {
+            normal = true;
+        }
     }
     void Update()
     {
-        /*if (player.position.y < 18)
-        {
-            transform.position = new Vector3(player.position.x, player.position.y + 3.33f, transform.position.z);
-        }
-        else
-        {*/
+        if (normal)
             transform.position = new Vector3(player.position.x, player.position.y + 3.5f, transform.position.z);
-        //} 
-            }
+        else
+        {
+            transform.position = new Vector3(player.position.x, nullpos, transform.position.z);
+        }
+    }
 }
