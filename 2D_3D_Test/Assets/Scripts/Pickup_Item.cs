ed1e231b660157a6aa29977c0da3c09f3d733908
Sven <Sven.Berzellis@t-online.de>
2018-02-25 19:21:20 +0100

    Pickup_Itemscript gebaut

---
+++ 2D_3D_Test/Assets/Scripts/Pickup_Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_Item : MonoBehaviour {
+
+    
+
+    public void  OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            other.setActive = false;
+        }
+    }
+}
